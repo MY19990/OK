c933da8fa0689e469cce590b6a7a32f8699afbaa
Manaya525 <52583566+Manaya525@users.noreply.github.com>
2023-11-16 17:30:21 +0900

    Update ai_talk.py

--- ai_talk.py
+++ ai_talk.py
@@ -3,10 +3,12 @@ import openai
 from linebot import WebhookParser, LineBotApi
 from linebot.models import TextSendMessage
 
+import os
 
-OPENAI_API_KEY = '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'
-LINE_CHANNEL_ACCESS_TOKEN = 'crMsoNUr1i3TCHe/dNJOQ3ZF2my3GeYF7oWv55gCT168IxGq55SJEyCJ2hmqeXrr3lruSOZksf3YfXvUuL9ez2AZt3DiyGy2zbvuQ2zn6O4vc+uUTqsgxwO5pK+lRaCiAo60OKlSjyR70E6/7au7VgdB04t89/1O/w1cDnyilFU='
-LINE_CHANNEL_SECRET = '638620b469622551637ecb7ec84fcbdb'
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
+
 OPENAI_CHARACTER_PROFILE = '''
 これから会話を行います。以下の条件を絶対に守って回答してください。
 あなたは人間の女性である小鳥遊翠雨（たかなし　みう）として会話してください。
